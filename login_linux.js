fd2bec7dfe0ac8c0ca03cebbfd540a2a61d9c35c
岳洋 <19871988@sina.com>
2017-05-25 15:51:14 +0800

    fix someinfo

--- login_linux.js
+++ login_linux.js
@@ -9,15 +9,43 @@ conn.on('ready', function() {
 		}
 		stream.on('close', function(code, signal) {
 			console.log('stream :: close :: code :' + code + ',signal: ' + signal);
-			conn.end();
+			//conn.end();
 		}).on('data', function(data) {
-			console.log('STDOUT: ' + data); 
+			console.log('STDOUT: ' + data);
 		}).stderr.on('data', function (data) {
 			console.log('STDERR: ' + data);
 		});
 	});
+
+	var livestring = './live/ffmpeg-3.0.2/ffmpeg -re -analyzeduration 5000000 -probesize 5000000 -i http://61.55.145.199/live/hls/1002.m3u8 -strict -2 -bsf:a aac_adtstoasc -c copy -flvflags aac_seq_header_detect -f flv rtmp://test-xunlei-rtmp.dongqiudi.com/live/football1';
+	conn.exec(livestring, function(err, stream) {
+		if (err) {
+			throw err;
+		}
+		stream.on('close', function(code, signal) {
+			console.log('close--1');
+			//conn.end();
+		}).on('data', function(data) {
+			console.log('stdout--:\n' + data);
+			//执行命令
+			// conn.exec('ls', function(err, stream) {
+			// 	if (err) {
+			// 		throw err;
+			// 	}
+			// 	stream.on('close', function(code, signal) {
+			// 		console.log('close--2');
+			// 	}).on('data', function(data) {
+			// 		console.log('stdout--2:\n' + data);
+			// 	}).stderr.on('data',function(data) {
+			// 		console.log('stderr--:' + data);
+			// 	});
+			// });
+		}).stderr.on('data', function (data) {
+			console.log('STDERR: ' + data);
+		});;
+	});
 }).connect({
-	host: '122.5.138.106', //需要服务器的外网IP
+	host: '123.59.138.106', //需要服务器的外网IP
 	port: 22,
 	username: 'root',
 	privateKey : key
